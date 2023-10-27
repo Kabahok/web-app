939a280cf72d35fa0502a28a274f75a87d664419
egor avdeev <workegoravd@gmail.com>
2023-10-27 14:06:48 +0300

    add di-annotation

--- src/main/java/org/egoravdeev/service/PostService.java
+++ src/main/java/org/egoravdeev/service/PostService.java
@@ -4,9 +4,12 @@ import org.egoravdeev.constants.StringConsts;
 import org.egoravdeev.exeption.NotFoundExeption;
 import org.egoravdeev.model.Post;
 import org.egoravdeev.repository.PostRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class PostService {
     private final PostRepository repository;
 
